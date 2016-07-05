56f9c7e8e70ad8d6b269ad00520f772f850185c0
Charlotte Smith <hello@seanjohnson.nz>
2016-07-05 14:59:02 +1200

    added more buttons

--- components/Counter.js
+++ components/Counter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {count: 0}
@@ -11,11 +11,16 @@ class Counter extends Component {
       this.setState({count: this.state.count + 1})
   }
 
+  decreaseCounter () {
+    this.setState({count: this.state.count - 1})
+  }
+
   render () {
     return (
       <div>
         <h4> Counter is now {this.state.count} </h4>
         <button onClick={this.increaseCounter.bind(this)}>Moar</button>
+        <button onClick={this.decreaseCounter.bind(this)}>Lass</button>
       </div>
     )
   }
